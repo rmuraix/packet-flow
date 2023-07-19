91d01e1a604eba1e5eec25ceeb0f54c88694b3d4
rmuraix <35632215+rmuraix@users.noreply.github.com>
2023-07-19 23:22:25 +0900

    Add test

--- src/handler/direction.rs
+++ src/handler/direction.rs
@@ -10,3 +10,23 @@ pub fn is_destination(ip: IpAddr, ips: Vec<IpAddr>) -> bool {
     }
     is_destination
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::net::Ipv4Addr;
+
+    #[test]
+    fn test_is_destination() {
+        let ip: IpAddr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+
+        let mut ips: Vec<IpAddr> = Vec::new();
+        ips.push(IpAddr::V4(Ipv4Addr::new(192, 168, 0, 1)));
+
+        assert_eq!(is_destination(ip, (*ips).to_vec()), false);
+
+        ips.push(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)));
+
+        assert_eq!(is_destination(ip, (*ips).to_vec()), true);
+    }
+}
